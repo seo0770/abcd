9f59b4af209b135112b41f3c9d5bdd54fbe73092
seo0770 <149846624+seo0770@users.noreply.github.com>
2023-11-04 10:49:44 +0900

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 st.title("ㅋㅋㄹㅃㅃ")
 dataframe = pd.DataFrame({"first column": [1,2,3,4],"second column": [10,20,30,40]})
-st.dataframe(dataframe,use_container_width=False)
-st.table(dataframe)
+st.dataframe(dataframe,use_container_width=False) #아래와 다르게 기능 존재
+st.table(dataframe) #그냥 출력.
 
 
 
