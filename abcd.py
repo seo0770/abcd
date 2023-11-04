3397c25b6ae2561007f2e79b5df5bffa94964a69
seo0770 <149846624+seo0770@users.noreply.github.com>
2023-11-04 10:39:31 +0900

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 st.title("ㅋㅋㄹㅃㅃ")
+dataframe = pd.DataFrame({"first column": [1,2,3,4],"second column": [10,20,30,40]})
+st.dataframe(dataframe,use_container_width=False)
+st.table(dataframe)
+
+
+
 st.write("Hello **world**!")
