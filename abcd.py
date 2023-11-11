78676071df79dce29cadf8d25aea7a2f97aabfd9
seo0770 <149846624+seo0770@users.noreply.github.com>
2023-11-11 09:29:58 +0900

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -9,10 +9,10 @@ st.table(dataframe) #그냥 출력.
 
 
 st.write("Hello **world**!")
-st.metric(label="온도",value="10℃",delta="1.2℃")
+st.metric(label="온도",value="10℃",delta="1.2℃") #label은 위에 글씨 , value = 중심에 글자,delta = 아래에 숫자(양수,음수는 서로 화살표 방향이 다름.)
 st.metric(label="삼성전자",value="61,000 원",delta="-1200 원")
 
-col1,col2,col3 = st.columns(3)
+col1,col2,col3 = st.columns(0) #가로줄을 세개로 나눔
 col1.metric(label="달러USB",value="1,228 원",delta="-32.00 원")
 col2.metric(label="일본JPY(100엔)",value="958.63 원",delta="-7.44 원")
 col3.metric(label="유럽연합EUR",value="1,335.82 원",delta="11.44 원")
