01b3bc9c1152f6e648e3230e0469eb208966d095
seo0770 <149846624+seo0770@users.noreply.github.com>
2023-11-11 09:54:08 +0900

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -14,7 +14,6 @@ with tab2:
   st.image("https://static.steamlit.io/examples/dog.jpg",width=200)
 
 dataframe = pd.DataFrame({"first column": [1,2,3,4],"second column": [10,20,30,40]})
-
 st.dataframe(dataframe,use_container_width=False) #아래와 다르게 기능 존재 , 인수use...는 크기를 가로에 맞춤?
 st.table(dataframe) #그냥 출력.
 
