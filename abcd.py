fdbeebda1368fd9491845d4d2ba49fe03bd3b60c
seo0770 <149846624+seo0770@users.noreply.github.com>
2023-11-11 09:51:33 +0900

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+st.title("ㅋㅋㄹㅃㅃ")
+
 tab1,tab2 = st.tabs(["Cat" , "Dog"])
 with tab1:
   st.header("A cat")
@@ -11,7 +13,6 @@ with tab2:
   st.header("A dog")
   st.image("https://static.steamlit.io/examples/dog.jpg",width=200)
 
-st.title("ㅋㅋㄹㅃㅃ")
 dataframe = pd.DataFrame({"first column": [1,2,3,4],"second column": [10,20,30,40]})
 st.dataframe(dataframe,use_container_width=False) #아래와 다르게 기능 존재 , 인수use...는 크기를 가로에 맞춤?
 st.table(dataframe) #그냥 출력.
