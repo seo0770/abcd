644942e0fb694fdb13f178a7b902443569c257aa
seo0770 <149846624+seo0770@users.noreply.github.com>
2023-11-11 09:38:39 +0900

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -14,6 +14,6 @@ st.metric(label="삼성전자",value="61,000 원",delta="-1200 원")
 
 col1,col2,col3 = st.columns(3) #가로줄을 세개로 나눔
 col1.metric(label="달러USB",value="1,228 원",delta="-32.00 원") #같은곳에 매트릭을 쓰면 그아래에 만들어짐.단 같은 내용일 경우 이미있는 위치에 써짐.
-col1.metric(label="달러",value="1,228 원",delta="-32.00 원")
+col1.metric(label="달러USB",value="1,11128 원",delta="-32.00 원")
 col2.metric(label="일본JPY(100엔)",value="958.63 원",delta="-7.44 원")
 col3.metric(label="유럽연합EUR",value="1,335.82 원",delta="11.44 원")
