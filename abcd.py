ed69fc183afcc91391fd854a454bd5b654ce2688
seo0770 <149846624+seo0770@users.noreply.github.com>
2023-11-11 09:19:35 +0900

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 st.title("ㅋㅋㄹㅃㅃ")
 dataframe = pd.DataFrame({"first column": [1,2,3,4],"second column": [10,20,30,40]})
-st.dataframe(dataframe,use_container_width=True) #아래와 다르게 기능 존재
+st.dataframe(dataframe,use_container_width=True) #아래와 다르게 기능 존재 , 인수use...는 크기를 가로에 맞춤?
 st.table(dataframe) #그냥 출력.
 
 
